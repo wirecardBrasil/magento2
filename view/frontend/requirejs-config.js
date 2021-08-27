fbd2256d539636d8d952c33e55c3784d612c88f6
elisei <brunoelisei@o2ti.com>
2021-08-27 12:15:19 -0300

    Moip ♥ Magento 2
    Fixed #93 #90

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -4,16 +4,13 @@
  * See COPYING.txt for license details.
  */
 var config = {
-     map: {
-        '*': {
-            'Magento_Checkout/js/action/select-payment-method':
-                'Moip_Magento2/js/action/payment/select-payment-method'
-        }
-    },
     config: {
         mixins: {
             "Magento_Payment/js/model/credit-card-validation/credit-card-number-validator/credit-card-type": {
                 "Moip_Magento2/js/mixins/credit-card-type-mixin": true
+            },
+            "Magento_Checkout/js/action/select-payment-method": {
+                "Moip_Magento2/js/mixins/select-payment-method-mixin": true
             }
         }
     }
