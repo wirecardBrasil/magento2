4f4d0b7fb900af0bd4dbed383c667afcdafb60bc
elisei <brunoelisei@o2ti.com>
2021-08-27 12:12:27 -0300

    Moip ♥ Magento 2
    Fixed #93

---
+++ view/base/web/js/mixins/select-payment-method-mixin.js
@@ -0,0 +1,36 @@
+/**
+ * Copyright © Wirecard Brasil. All rights reserved.
+ * @author    Bruno Elisei <brunoelisei@o2ti.com>
+ * See COPYING.txt for license details.
+ */
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+define([
+    "jquery",
+    "mage/utils/wrapper",
+    "Moip_Magento2/js/action/checkout/cart/totals"
+], function ($, wrapper, totalsMoipInterest) {
+    "use strict";
+
+    return function (selectPaymentMethodAction) {
+
+        return wrapper.wrap(selectPaymentMethodAction, function (originalSelectPaymentMethodAction, paymentMethod) {
+
+            originalSelectPaymentMethodAction(paymentMethod);
+
+            if (paymentMethod === null) {
+                return;
+            }
+
+            if(window.checkoutConfig.payment["moip_magento2_cc"].isActive){
+                if(window.checkoutConfig.payment["moip_magento2_cc"].info_interest[1] < 0){
+                    totalsMoipInterest.save(0);
+                }
+            }
+            return;
+        });
+    };
+
+});
